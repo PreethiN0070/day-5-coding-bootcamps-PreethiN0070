0b69fa99dc1181cc709d9bb99c3647d3a51c4ec0
PreethiN0070 <Preethinarayan2003@gmail.com>
2021-02-19 12:23:17 +0000

    Preethi N and Rathan M C 5th program

--- Program_3.c
+++ Program_3.c
@@ -6,12 +6,29 @@
 //output:
 //Palindrome
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
 	char str[10];
 	scanf("%s", str);
-
+ int i,len,count=0;
+ len = strlen(str);
+ for(i=0;i<len;i++)
+ {
+   if(str[i]!=str[len-i-1]){
+   count=1;
+   break;
+   }
+   
+ }
+if (count==0){
+  printf("the string is a palindrome\n");
+  
+}
+else{
+  printf("the given string is not a palindrome\n");
+}
+ 
 	//Write your code here
 	
 	return 0;
